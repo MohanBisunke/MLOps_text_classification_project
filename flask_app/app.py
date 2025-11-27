b67b95c6bcb783da8d0cd2a7a304fad66e8819a4
Mohan <bisunkemohan@gmail.com>
2025-11-27 13:09:14 +0545

    Model Promotion & Testing code check -1

--- flask_app/app.py
+++ flask_app/app.py
@@ -167,5 +167,5 @@ def metrics():
     return generate_latest(registry), 200, {"Content-Type": CONTENT_TYPE_LATEST}
 
 if __name__ == "__main__":
-    app.run(debug=True) # for local use
-    # app.run(debug=True, host="0.0.0.0", port=5000)  # Accessible from outside Docker
+    # app.run(debug=True) # for local use
+    app.run(debug=True, host="0.0.0.0", port=5000)  # Accessible from outside Docker
---
+++ scripts/promote_model.py
@@ -0,0 +1,42 @@
+import os
+import mlflow
+
+
+def promote_model():
+    # Set up DagsHub credentials for MLflow tracking
+    dagshub_token = os.getenv("DAGSHUB_TOKEN")
+    if not dagshub_token:
+        raise EnvironmentError("DAGSHUB environment variable is not set")
+
+    os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+    dagshub_url = "https://dagshub.com"
+    repo_owner = "MohanBisunke"
+    repo_name = "MLOps_text_classification_project"
+    # Set up MLflow tracking URI
+    mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+    
+    client = mlflow.MlflowClient()
+    model_name = "my_model"
+    latest_version_staging = client.get_latest_versions(model_name, stages =['Staging'])[0].version
+    
+    prod_version = client.get_latest_versions(model_name, stages=['Production'])
+    
+    for version in prod_version:
+        client.transition_model_version_stage(
+            name= model_name,
+            version = version.version,
+            stage= "Archived"
+        )
+        
+    client.transition_model_version_stage(
+        name=model_name,
+        version=latest_version_staging,
+        stage="Production"
+    )
+    
+    print(f"Model version {latest_version_staging} promoted to Production")
+
+if __name__ == "__main__":
+    promote_model()
